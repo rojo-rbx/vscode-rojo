226076418a17ca4acb5a9a70c8c4f4bda3b48e0c
Eryn Lynn <eryn@eryn.io>
2021-02-19 15:03:22 -0500

    Add error handling to start command

--- src/commands/Start.ts
+++ src/commands/Start.ts
@@ -12,41 +12,47 @@ import { pickRojo } from "../util/pickRojo"
 export const startCommand = vscode.commands.registerCommand(
   "rojo.start",
   async () => {
-    const bridge = await getBridge()
-    // Get the correct Rojo instance the user wants to start.
-    const rojo = await pickRojo({
-      noFoldersError:
-        "Rojo can only start if VS Code is opened on a workspace folder.",
-      prompt: "Select a workspace to start Rojo in."
-    })
-    // Will be undefined if user closed the box
-    if (!rojo || !bridge) return
-
     try {
-      // Wrapped in a try/catch because child_process.spawn can get some strange errors sometimes (like EBUSY)
-      rojo.serve()
-    } catch (e) {
-      console.log(e)
-      Telemetry.trackEvent(
-        TelemetryEvent.RuntimeError,
-        "Rojo spawn exception",
-        os.platform()
-      )
-      Telemetry.trackException(e)
-      vscode.window.showErrorMessage("An error occurred while spawning Rojo.")
-      return
-    }
-    // One final check to make sure that `rojo.serving` is true. If it's not, something caused the serve function to stop prematurely.
-    if (!rojo.serving) {
-      vscode.window.showErrorMessage("An error occurred while spawning Rojo.")
-      Telemetry.trackEvent(
-        TelemetryEvent.RuntimeError,
-        "Rojo not serving",
-        os.platform()
-      )
-    }
+      const bridge = await getBridge()
+      // Get the correct Rojo instance the user wants to start.
+      const rojo = await pickRojo({
+        noFoldersError:
+          "Rojo can only start if VS Code is opened on a workspace folder.",
+        prompt: "Select a workspace to start Rojo in."
+      })
+      // Will be undefined if user closed the box
+      if (!rojo || !bridge) return
 
-    // Add the saved Rojo version to the button while setting it to Running.
-    statusButton.setState(ButtonState.Running, bridge.version)
+      try {
+        // Wrapped in a try/catch because child_process.spawn can get some strange errors sometimes (like EBUSY)
+        rojo.serve()
+      } catch (e) {
+        console.log(e)
+        Telemetry.trackEvent(
+          TelemetryEvent.RuntimeError,
+          "Rojo spawn exception",
+          os.platform()
+        )
+        Telemetry.trackException(e)
+        vscode.window.showErrorMessage("An error occurred while spawning Rojo.")
+        return
+      }
+      // One final check to make sure that `rojo.serving` is true. If it's not, something caused the serve function to stop prematurely.
+      if (!rojo.serving) {
+        vscode.window.showErrorMessage("An error occurred while spawning Rojo.")
+        Telemetry.trackEvent(
+          TelemetryEvent.RuntimeError,
+          "Rojo not serving",
+          os.platform()
+        )
+      }
+
+      // Add the saved Rojo version to the button while setting it to Running.
+      statusButton.setState(ButtonState.Running, bridge.version)
+    } catch (e) {
+      vscode.window.showErrorMessage(
+        `An error occurred while starting Rojo: ${e}`
+      )
+    }
   }
 )
